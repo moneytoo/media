62b71095df87509c3509bcf1e5fd9edf532b32aa
Marcel Dopita <m@rcel.cz>
2022-12-04 15:53:38 +0100

    Controller progress bar

--- libraries/ui/src/main/java/androidx/media3/ui/PlayerControlView.java
+++ libraries/ui/src/main/java/androidx/media3/ui/PlayerControlView.java
@@ -1250,6 +1250,10 @@ public class PlayerControlView extends FrameLayout {
     controlViewLayoutManager.show();
   }
 
+  public void showProgress() {
+    controlViewLayoutManager.showProgress();
+  }
+
   /** Hides the controller. */
   public void hide() {
     controlViewLayoutManager.hide();
