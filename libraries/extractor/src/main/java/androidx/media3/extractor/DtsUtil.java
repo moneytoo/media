1c76f93c00453b96765f16af18b77fef00ae994f
Marcel Dopita <m@rcel.cz>
2022-12-04 15:57:29 +0100

    Add option to map DV profile 7 to HEVC

--- libraries/extractor/src/main/java/androidx/media3/extractor/DtsUtil.java
+++ libraries/extractor/src/main/java/androidx/media3/extractor/DtsUtil.java
@@ -207,6 +207,20 @@ public final class DtsUtil {
       };
 
   /**
+   * Returns whether a given integer matches a DTS sync word. Synchronization and storage modes are
+   * defined in ETSI TS 102 114 V1.1.1 (2002-08), Section 5.3.
+   *
+   * @param word An integer.
+   * @return Whether a given integer matches a DTS sync word.
+   */
+  public static boolean isSyncWord(int word) {
+    return word == SYNC_VALUE_BE
+        || word == SYNC_VALUE_LE
+        || word == SYNC_VALUE_14B_BE
+        || word == SYNC_VALUE_14B_LE;
+  }
+
+  /**
    * Maps MaxSampleRate index to sampling frequency in Hz. See ETSI TS 102 114 V1.6.1 (2019-08)
    * Table 7-9.
    */
