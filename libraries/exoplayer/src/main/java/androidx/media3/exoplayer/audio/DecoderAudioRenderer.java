82a94f69d480da294677f5654f394679d60a132e
tonihei <tonihei@google.com>
2025-05-27 18:09:53 +0200

    Add missing renderer capabilities changed forwarding
    Issue: androidx/media#2258
    PiperOrigin-RevId: 763819705
    
    (cherry picked from commit f825ad7e476c7190f21ff0bddb8e02ddf9a5a856)

--- libraries/exoplayer/src/main/java/androidx/media3/exoplayer/audio/DecoderAudioRenderer.java
+++ libraries/exoplayer/src/main/java/androidx/media3/exoplayer/audio/DecoderAudioRenderer.java
@@ -935,6 +935,11 @@ public abstract class DecoderAudioRenderer<
     public void onAudioTrackReleased(AudioSink.AudioTrackConfig audioTrackConfig) {
       eventDispatcher.audioTrackReleased(audioTrackConfig);
     }
+
+    @Override
+    public void onAudioCapabilitiesChanged() {
+      DecoderAudioRenderer.this.onRendererCapabilitiesChanged();
+    }
   }
 
   @RequiresApi(23)
